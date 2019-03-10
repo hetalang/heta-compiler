7a49cd46521210bde5ead111d816320e6e70131d
Evgeny Metelkin <metelkin@insysbio.ru>
2019-03-10 21:20:32 +0300

    update index file

--- src/index.js
+++ src/index.js
@@ -1,26 +1,7 @@
-const { Quantity } = require('./core/quantity');
-const { Compartment } = require('./core/compartment');
-const { Reaction } = require('./core/species');
-const { Species } = require('./core/species');
-const { Model } = require('./core/model');
-const { Numeric } = require('./core/numeric');
-const { Expression } = require('./core/expression');
-const { ReferenceDefinition } = require('./core/reference-definition');
-
 // set sbml methods
 require('./sbml/model');
 require('./sbml/quantity');
 
 const { Container } = require('./container');
 
-module.exports = {
-  Quantity,
-  Compartment,
-  Species,
-  Reaction,
-  Container,
-  Model,
-  Numeric,
-  Expression,
-  ReferenceDefinition
-};
+module.exports = Container;
