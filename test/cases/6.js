b37097cdd65797ab1a6f2b9e731da6b7c2dc4c95
Evgeny Metelkin <metelkin@insysbio.com>
2019-11-27 13:50:25 +0300

    add test for use action

---
+++ test/cases/6.js
@@ -0,0 +1,56 @@
+/* global describe, it */
+
+const { Builder } = require('../../src');
+const { expect, use } = require('chai');
+const chaiXml = require('chai-xml');
+use(chaiXml);
+const fs = require('fs-extra');
+
+const sbml_correct = fs.readFileSync('cases/6-use-case/master/output_sbml.xml','utf8');
+const json_correct = require('../../cases/6-use-case/master/output_json.json');
+
+describe('Testing "cases/6-use-case"', () => {
+  let b;
+
+  it('Create builder.', () => {
+    let declaration = {
+      'id': 'test',
+      'builderVersion': '^0.4',
+      'options': {
+        'logLevel': 'error'
+      },
+      'importModule': {
+        'type': 'heta',
+        'source': 'src/index.heta'
+      }
+    };
+    b = new Builder(declaration, 'cases/6-use-case', '../../test/cases/6/dist');
+    //console.log(b);
+  });
+
+  it('Run include', async () => {
+    await b.compileAsync();
+  });
+
+  it('Run @SBMLExport, check and compare.', () => {
+    let sbml_export = b.container.select({id: 'output_sbml', space: 'model'});
+    let code = sbml_export.do();
+    expect(code).xml.to.to.be.valid();
+    expect(code).xml.be.deep.equal(sbml_correct);
+    //console.log(code);
+  });
+
+  it('Run @JSONExport, check and compare.', () => {
+    const JSONExport = b.container.classes.JSONExport;
+    let json_export = new JSONExport;
+    json_export._id = 'output_json';
+    json_export._space = 'model';
+    json_export._container = b.container;
+
+    let code = json_export.do();
+    let obj = JSON.parse(code);
+    expect(obj).to.be.deep.equal(json_correct);
+    //console.log(obj);
+  });
+});
+  
