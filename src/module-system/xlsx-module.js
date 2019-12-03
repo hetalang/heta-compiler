7bde2eed050bb409cf6e9ff965f89c76ef0ae555
Evgeny Metelkin <metelkin@insysbio.com>
2019-12-03 18:35:22 +0300

    fix error in excel-as-json

--- src/module-system/xlsx-module.js
+++ src/module-system/xlsx-module.js
@@ -1,6 +1,8 @@
 const _Module = require('./_module');
 const { processFile } = require('excel-as-json'); // see https://www.npmjs.com/package/excel-as-json
 const _ = require('lodash');
+
+// to use as Promise
 const util = require('util');
 const convertExcel = util.promisify(processFile);
 
@@ -12,7 +14,20 @@ _Module.prototype.setXLSXModuleAsync = async function(){
     waitSec: 10
   });
 
-  let rawData = await convertExcel(this.filename, null, {sheet: options.sheet, omitEmptyFields: true});
+  // this part is the way to fix bug in excel-as-json 10 second before callback
+  // throws error in 10 seconds in any way
+  let waiter = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let err = new Error(`sheet #${options.sheet} is not found in "${this.filename}" or reading table require more than ${options.waitSec} sec.`);
+      reject(err);
+    }, 1000*options.waitSec);
+  });
+
+  // who is faster
+  let rawData = await Promise.race([
+    convertExcel(this.filename, null, { sheet: options.sheet, omitEmptyFields: true }),
+    waiter
+  ]);
   rawData.splice(0, options.omitRows); // remove rows
 
   let dataFiltered = rawData
