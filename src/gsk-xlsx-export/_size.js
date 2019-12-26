2524095dbc4724118ab3747c0925c17c76a96f5e
Evgeny Metelkin <metelkin@insysbio.com>
2019-12-26 21:55:51 +0300

    change gsk export

--- src/gsk-xlsx-export/_size.js
+++ src/gsk-xlsx-export/_size.js
@@ -1,14 +1,15 @@
 const { _Size } = require('../core/_size');
-const qspToGSK = require('./qsp-to-gsk');
+//const qspToGSK = require('./qsp-to-gsk');
 
 _Size.prototype.toFlat = function(options = {}){
   //let res = super.toFlat(options);
   let res = _Size.__proto__.prototype.toFlat.call(this, options);
-  res.unitsGSK = this.unitsGSK();
+  //res.unitsGSK = this.unitsGSK();
+  res.unitsGSK = this.units;
 
   return res;
 };
-
+/*
 _Size.prototype.unitsGSK = function(){
   return this.unitsParsed
     ? this.unitsParsed
@@ -16,3 +17,4 @@ _Size.prototype.unitsGSK = function(){
       .toString()
     : undefined;
 };
+*/
