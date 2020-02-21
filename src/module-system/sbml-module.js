16025800ad6d5d2051808c3b5be4484ee1dac47b
Evgeny Metelkin <metelkin@insysbio.com>
2020-02-21 19:53:13 +0300

    add base element to SML module

--- src/module-system/sbml-module.js
+++ src/module-system/sbml-module.js
@@ -23,6 +23,9 @@ function _SBMLParse(filename, fileContent){
   }
 }
 
+/*
+  Converst of JSON image of SBML to Heta array
+*/
 function jsbmlToQArr(JSBML){
   let qArr = [];
 
@@ -56,43 +59,126 @@ function jsbmlToQArr(JSBML){
     qArr.push(q);
   });
 
-  console.log(parameters);
+  //console.log(parameters[0]);
   return qArr;
 }
 
-function compartmentToQ(x){
-  let start_ = _.get(x, 'attributes.size');
+/*
+  Converts common properties to Heta Object
+*/
+function baseToQ(x){
   let q = {
     id: _.get(x, 'attributes.id'),
-    class: 'Compartment',
-    title: _.get(x, 'attributes.name'),
-    boundary: _.get(x, 'attributes.constant') !== 'false'
+    title: _.get(x, 'attributes.name')
   };
-  if (start_ !== undefined) q.assignments = { start_: start_ };
+  // set metaid
+  let metaid = _.get(x, 'attributes.metaid');
+  if (metaid !== undefined) _.set(q, 'aux.metaid', metaid);
+  // set sboTerm
+  let sboTerm = _.get(x, 'attributes.sboTerm');
+  if (sboTerm !== undefined) _.set(q, 'aux.sboTerm', sboTerm);
+  // take only first notes
+  let notes = x.elements
+    && x.elements.find((y) => y.name === 'notes');
+  if (notes) q.notes = _toMarkdown(notes.elements);
+  // annotation
+  let annotation = x.elements
+    && x.elements.find((y) => y.name === 'annotation');
+  if (annotation) _.set(q, 'aux.annotation', _toAux(annotation.elements));
+
+  return q;
+}
+
+function _toMarkdown(elements){
+  let text = elements.map((x) => {
+    if (x.type === 'text') {
+      return x.text.replace(/\r*\n/g, '');
+    } else {
+      switch (x.name) {
+        case 'body':
+        case 'div':
+        case 'p':
+          return _toMarkdown(x.elements) + '\n\n';
+          break;
+        case 'b':
+        case 'strong':
+          return '**' + _toMarkdown(x.elements) + '**';
+          break;
+        case 'i':
+          return '_' +_toMarkdown(x.elements) + '_';
+          break;
+        case 'ul': {
+          let list = x.elements.map((y) => '  * ' +_toMarkdown(y.elements))
+            .join('\n');
+          return list;
+          break;
+        }
+        case 'ol': {
+          let list = x.elements.map((y) => '  1. ' +_toMarkdown(y.elements))
+            .join('\n');
+          return list;
+          break;
+        }
+        case 'a':
+          let href = _.get(x, 'attributes.href');
+          let title = _.get(x, 'attributes.title');
+          return '[' + _toMarkdown(x.elements) + '](' + href + ')';
+          break;
+        case 'h1':
+          return '# ' + _toMarkdown(x.elements) + '\n\n';
+          break;
+        case 'h2':
+          return '## ' + _toMarkdown(x.elements) + '\n\n';
+          break;
+        case 'h3':
+          return '### ' + _toMarkdown(x.elements) + '\n\n';
+          break;
+        case 'h4':
+          return '#### ' + _toMarkdown(x.elements) + '\n\n';
+          break;
+        case 'h5':
+          return '##### ' + _toMarkdown(x.elements) + '\n\n';
+          break;
+        case 'h6':
+          return '###### ' + _toMarkdown(x.elements) + '\n\n';
+          break;
+      default:
+          return _toMarkdown(x.elements);
+      }
+    }
+  }).join('');
+
+  return text;
+}
+
+// TODO: use the same result as in SBMLViewer
+function _toAux(elements){
+  return elements;
+}
+
+function compartmentToQ(x){
+  let q = baseToQ(x);
+
+  q.class = 'Compartment';
+  q.boundary = _.get(x, 'attributes.constant') !== 'false';
+  let start_ = _.get(x, 'attributes.size');
+  if (start_ !== undefined) _.set(q, 'assignments.start_', start_);
 
   return q;
 }
 
 function parameterToQ(x){
+  let q = baseToQ(x);
+
   let isConstant = _.get(x, 'attributes.constant') === 'true';
   if (isConstant) {
+    q.class = 'Const';
     let num = _.get(x, 'attributes.value');
-    var q = {
-      id: _.get(x, 'attributes.id'),
-      class: 'Const',
-      title: _.get(x, 'attributes.name') 
-    };
     if (num !== undefined) q.num = Number.parseFloat(num);
   } else {
+    q.class = 'Record';
     let start_ = _.get(x, 'attributes.value');
-
-    q = {
-      id: _.get(x, 'attributes.id'),
-      class: 'Record',
-      title: _.get(x, 'attributes.name')
-    };
-
-    if (start_ !== undefined) q.assignments = { start_: start_ };
+    if (start_ !== undefined) _.set(q, 'assignments.start_', start_);
   }
 
   return q;
