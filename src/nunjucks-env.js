db0e46bd1e80b81d4ae2724cbb6cb8db63b0ca89
Evgeny Metelkin <evgeny.metelkin@gmail.com>
2020-04-28 14:30:30 +0300

    set general paths in nunjacks

--- src/nunjucks-env.js
+++ src/nunjucks-env.js
@@ -1,15 +1,22 @@
 const nunjucks = require('nunjucks');
 const _ = require('lodash');
 
+// initial version: loading templates based on relative path of the package
+// FileSystemLoader() works only in Node
+/*  
 const env = new nunjucks.Environment(
   new nunjucks.FileSystemLoader(__dirname),
   { autoescape: false }
 );
-/*
-env.addFilter('selectattr2', function(arr, attr, value) {
-  return arr.filter((x) => x[attr]===value);
-});
 */
+
+// this version will look in "node_packages/heta-compiler/src" in node
+// and https://homepage.com/ in browser
+const env = nunjucks.configure(
+  __dirname,
+  { autoescape: false }
+);
+
 env.addFilter('filter2', function(arr, path, value) {
   return arr.filter((x) => _.get(x, path)===value);
 });
