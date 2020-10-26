a0401d9cc948b2cca118897e5ff04e4121e12b33
Evgeny Metelkin <evgeny.metelkin@gmail.com>
2020-10-26 15:27:08 +0300

    ad atStart prop to Switcher

--- src/core/_switcher.js
+++ src/core/_switcher.js
@@ -1,25 +1,48 @@
 const { Component } = require('./component');
+const _ = require('lodash');
 
 /*
   _Switcher abstract class
 
   _switcher @_Switcher {
-    // no specific properties
+    atStart: true
   };
 */
 class _Switcher extends Component {
   merge(q = {}){
     super.merge(q);
-    //let logger = _.get(this, 'namespace.container.logger');
-    //let valid = _Switcher.isValid(q, logger);
+    let logger = _.get(this, 'namespace.container.logger');
+    let valid = _Switcher.isValid(q, logger);
+
+    if (valid) {
+      if (typeof q.atStart !== 'undefined') {
+        this.atStart = q.atStart;
+      }
+    }
 
     return this;
   }
+  clone(){
+    let clonedComponent = super.clone();
+    if (typeof this.atStart !== 'undefined')
+      clonedComponent.atStart = this.atStart;
+      
+    return clonedComponent;
+  }
   toQ(options = {}){
     let res = super.toQ(options);
+    if (this.atStart) res.atStart = true;
+
     return res;
   }
 }
+
+_Switcher._requirements = {
+  atStart: {
+    required: false, 
+    isReference: false
+  }
+};
 
 module.exports = {
   _Switcher
