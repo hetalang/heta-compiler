7842b0db9cf55c98a3ea5a8889fb2e66341bae3c
Evgeny Metelkin <evgeny.metelkin@gmail.com>
2021-02-08 15:01:10 +0300

    fix rebase test

--- test/unit/rebase.js
+++ test/unit/rebase.js
@@ -32,43 +32,43 @@ describe('unit transformation 0', () => {
   });
 
   it('Elementary unit transformation', () => {
-    let meter = c.select({id: 'meter'});
+    let meter = c.unitDefStorage.get('meter');
 
     expect(() => meter.unitsParsed.rebase(['meter', 'liter'])).to.throw(TypeError);
   });
 
   it('Simple unit tranformation.', () => {
-    let liter = c.select({id: 'liter'});
+    let liter = c.unitDefStorage.get('liter');
     let res0 = liter.unitsParsed.rebase(['meter', 'kilogram']);
     expect(res0.toString()).to.be.equal('(1e-1 meter)^3');
   });
 
   it('Simple unit tranformation error.', () => {
-    let liter = c.select({id: 'liter'});
+    let liter = c.unitDefStorage.get('liter');
     expect(() => liter.unitsParsed.rebase(['liter', 'kilogram']))
       .to.throw(TypeError);
   });
 
   it('Complex unit transformation 1', () => {
-    let xxx = c.select({id: 'xxx'});
+    let xxx = c.unitDefStorage.get('xxx');
     let res0 = xxx.unitsParsed.rebase(['kilogram', 'liter', 'meter']);
     expect(res0.toString()).to.be.equal('(1e-3 meter)^2/liter');
   });
 
   it('Complex unit transformation 2', () => {
-    let xxx = c.select({id: 'xxx'});
+    let xxx = c.unitDefStorage.get('xxx');
     let res0 = xxx.unitsParsed.rebase(['kilogram', 'meter']);
     expect(res0.toString()).to.be.equal('(1e-3 meter)^2/(1e-1 meter)^3');
   });
 
   it('Complex unit transformation error', () => {
-    let xxx = c.select({id: 'xxx'});
+    let xxx = c.unitDefStorage.get('xxx');
     expect(() => xxx.unitsParsed.rebase(['kilogram', 'liter']))
       .to.throw(Error);
   });
 
 });
-/*
+
 const platform1 = [
   { id: 'second', action: 'defineUnit' },
   { id: 'hour', action: 'defineUnit', units: [{ kind: 'second', exponent: 1, multiplier: 3600 }] },
@@ -86,19 +86,19 @@ describe('Hour problem testing', () => {
   });
 
   it('SBML-like transformation for UnitDef', () => {
-    let h = c.select({id: 'h'});
+    let h = c.unitDefStorage.get('h');
     let res0 = h.unitsParsed.rebase(['second']);
     expect(res0.toString()).to.be.equal('(3.6e+3 second)');
   });
 
   it('Simbio-like transformation for UnitDef', () => {
-    let h = c.select({id: 'h'});
+    let h = c.unitDefStorage.get('h');
     let res0 = h.unitsParsed.rebase(['hour']);
     expect(res0.toString()).to.be.equal('hour');
   });
 
   it('Error transformation for UnitDef', () => {
-    let h = c.select({id: 'h'});
+    let h = c.unitDefStorage.get('h');
     expect(() => h.unitsParsed.rebase(['week'])).to.throw(Error);
   });
 
@@ -120,4 +120,3 @@ describe('Hour problem testing', () => {
     expect(() => kel.unitsParsed.rebase(['week'])).to.throw(TypeError);
   });
 });
-*/
