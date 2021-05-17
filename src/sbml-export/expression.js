464a341b7eec5a983caf06f18ab642ec0778d5e1
Evgeny Metelkin <evgeny.metelkin@gmail.com>
2021-05-17 17:34:18 +0300

    update cmathml

--- src/sbml-export/expression.js
+++ src/sbml-export/expression.js
@@ -1,16 +1,16 @@
 const { Expression } = require('../core/expression');
 
-const math = require('mathjs');
-const mathjsMathML = require('mathjs-mathml');
-math.import(mathjsMathML);
+const { cMathMLHandler } = require('mathjs-mathml');
 
 const csymbols = {
   t: 'http://www.sbml.org/sbml/symbols/time'
 };
 
 Expression.prototype.toCMathML = function(skipHeader = false){
+  let baseMathML = this.exprParsed
+    .toString({ handler: cMathMLHandler, csymbols: csymbols });
 
   return !skipHeader
-    ? this.exprParsed.toCMathML(csymbols)
-    : this.exprParsed.toCMathMLNode(csymbols); // without <math>...</math>
+    ? `<math xmlns="http://www.w3.org/1998/Math/MathML">${baseMathML}</math>`
+    : baseMathML;
 };
