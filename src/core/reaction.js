130b8c83c30ae4117386a4481cc9ce947bfb0115
Evgeny Metelkin <metelkin@insysbio.com>
2021-05-28 12:05:36 +0300

    draft heta code export

--- src/core/reaction.js
+++ src/core/reaction.js
@@ -53,13 +53,13 @@ class Reaction extends Process {
   }
   toQ(options = {}){
     let res = super.toQ(options);
-    if(this.modifiers.length>0){
+    if (this.modifiers.length > 0) {
       res.modifiers = options.simplifyModifiers
         ? this.modifiers.map((modifier) => modifier.target )
         : this.modifiers.map((modifier) => { return { target: modifier.target }; });
     }
 
-    if (this.compartment) res.compartment = this.compartment;
+    if (this.compartment !== undefined) res.compartment = this.compartment;
     if (this.isAmount !== true) res.isAmount = this.isAmount;
 
     return res;
