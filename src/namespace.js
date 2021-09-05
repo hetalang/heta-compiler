68fe08d336c79dd412f947c4d95c2c510382f585
Evgeny Metelkin <metelkin@insysbio.com>
2021-09-05 12:28:45 +0300

    api docs for namespace

--- src/namespace.js
+++ src/namespace.js
@@ -2,27 +2,51 @@ const TopoSort = require('@insysbio/topo-sort');
 const _ = require('lodash');
 const { flatten } = require('./core/utilities');
 
-// implicit property container, XXX: should be renamed to _container
 class Namespace extends Map {
+  /**
+   * Namespace is a `Map` which stores components.
+   * 
+   * @extends Map
+   * 
+   * @param {string} spaceName Space identifier.
+   */
   constructor(spaceName){ 
     super();
     //if (typeof spaceName !== 'string')
     //  throw new TypeError(`spaceName argument must be string, got ${spaceName}`);
     this._spaceName = spaceName;
   }
+  /**
+   * `true` if namespace is abstract, `false` if concrete.
+   */
   get isAbstract(){
     return this._isAbstract;
   }
-  // title of space
+  /**
+   * Identifier of a namespace.
+   */
   get spaceName(){
     return this._spaceName;
   }
   set(key, value){
     return super.set(key, value);
   }
+  /**
+   * Converts namespace into array of components.
+   * 
+   * @returns {Component[]} All components in array format.
+   */
   toArray(){
     return [...this].map((x) => x[1]);
   }
+  /**
+   * Converts namespace into queue array.
+   * 
+   * @param {boolean} removeCoreComponents=true 
+   * @param {object} options Options passed to {@link Top#toQ} method
+   * 
+   * @returns {object[]} Array of JS objects in queue format.
+   */
   toQArr(removeCoreComponents = false, options = {}){
     let qArr = this.toArray()
       .filter((x) => !(removeCoreComponents && x.isCore))
@@ -30,6 +54,12 @@ class Namespace extends Map {
     
     return qArr;
   }
+  /**
+   * Representation of namespace in queue format.
+   * 
+   * @param {object} options reserved for future versions.
+   * @returns {object} JS object in queue format.
+   */
   toQ(options = {}){
     let res = {
       action: 'setNS',
@@ -39,24 +69,55 @@ class Namespace extends Map {
 
     return res; 
   }
+
+  /**
+   * Representation of namespace in flat queue format.
+   * 
+   * @param {object} options reserved for future versions.
+   * 
+   * @returns {object} JS object in flat queue format.
+   */
   toFlat(options = {}){
     let q = this.toQ(options);
     let res = flatten(q);
 
     return res; 
   }
+
+  /**
+   * Components of the particular class.
+   * 
+   * @param {string} className One of available Heta classes: 'Const', 'Record', etc.
+   * 
+   * @returns {Component[]} Array of selected components.
+   */
   selectByClassName(className){
     return this
       .toArray()
       .filter((x) => x.className === className);
   }
+
+  /**
+   * Components of the class and its inheritors.
+   * 
+   * @param {string} className One of available Heta classes: 'Const', 'Record', etc.
+   * 
+   * @returns {Component[]} Array of selected components.
+   */
   selectByInstanceOf(className){
     return this
       .toArray()
       .filter((x) => x.instanceOf(className));
   }
-  /*
-  */
+
+  /**
+   * Select all records from namespace and sort them by the scope.
+   * 
+   * @param {string} context scope of `Record`.
+   * @param {boolean} includeCompartmentDep=true
+   * 
+   * @returns {Component[]} Records in scope order.
+   */
   sortExpressionsByContext(context, includeCompartmentDep = false){
     // create topo-sort tree
     let graph = new TopoSort();
@@ -98,10 +159,25 @@ class Namespace extends Map {
 
     return sorted;
   }
+
+  /**
+   * 
+   * Select all records which have the scope.
+   * 
+   * @param {string} context scope of `Record`.
+   * 
+   * @returns {Component[]} Records that have the assignment associated with scope.
+   */
   selectRecordsByContext(context){
     return this.selectByInstanceOf('Record')
       .filter((record) => _.has(record, 'assignments.' + context));
   }
+
+  /**
+   * Select all units mentioned in namespace.
+   * 
+   * @returns {Unit[]} Array of units.
+   */
   getUniqueUnits(){
     return _
       .chain(this.selectByInstanceOf('_Size'))
@@ -110,6 +186,12 @@ class Namespace extends Map {
       .map((record) => record.unitsSBML())
       .value();
   }
+
+  /**
+   * Sequential checking and binding components.
+   * 
+   * @returns {Namespace} Self.
+   */
   knit(){
     this.toArray().forEach((component) => { // iterates all components
       component.bind(this);
@@ -117,7 +199,15 @@ class Namespace extends Map {
     
     return this;
   }
+
   // This will be done again in _Export.getXXXImage()
+  /**
+   * Check if there are circular assignments for the `scope`.
+   * Write errors in logger.
+   * 
+   * @param {string} scope selected assignment scope.
+   * @param {boolean} includeCompartmentDep=true To take into account Compartment (for `Species` only).
+   */
   checkCircRecord(scope, includeCompartmentDep = false){
     let logger = this.container.logger;
     try {
