49d4a3498794c0d8cba8f79c62a30d2dca760c7d
Evgeny Metelkin <evgeny.metelkin@gmail.com>
2025-08-25 15:26:57 +0300

    draft export CanonicalJSON

---
+++ src/canonical-json-export/index.js
@@ -0,0 +1,70 @@
+const { AbstractExport } = require('../abstract-export');
+const { ajv } = require('../ajv');
+const { omitByPaths } = require('../utils');
+
+const schema = {
+  type: 'object',
+  properties: {
+    omit: {type: 'array', items: { type: 'string' }},
+    noUnitsExpr: {type: 'boolean'}
+  }
+};
+
+class CanonicalJSONExport extends AbstractExport {
+  constructor(q = {}, isCore = false){
+    super(q, isCore);
+    
+    // check arguments here
+    let { logger } = this._builder;
+    let valid = CanonicalJSONExport.isValid(q, logger);
+    if (!valid) { this.errored = true; return; }
+
+    if (q.omit) this.omit = q.omit;
+    if (q.noUnitsExpr) this.noUnitsExpr = q.noUnitsExpr;
+  }
+  get className(){
+    return 'CanonicalJSONExport';
+  }
+  get defaultFilepath() {
+    return 'canonical';
+  }
+  get format(){
+    return 'CanonicalJSON';
+  }
+  static get validate(){
+    return ajv.compile(schema);
+  }
+  makeText(){
+    // filtered namespaces
+    let nsArrayFiltered = this.selectedNamespaces();
+
+    // create qArr from NS
+    let qArr_ns = nsArrayFiltered.reduce((accumulator, [spaceName, ns]) => {
+      let qArr_setns = ns.spaceName === 'nameless' && !ns.isAbstract ? [] : [ns.toQ()]; // skip #setNS {space: nameless};
+      let qArr_components = ns.toQArr(true, { noUnitsExpr: this.noUnitsExpr });
+      return accumulator.concat(qArr_setns, qArr_components);
+    }, []);
+    let qArr_unitDef = [...this._builder.container.unitDefStorage]
+      .filter((x) => !x[1].isCore)
+      .map((x) => x[1].toQ());
+    let qArr_functionDef = [...this._builder.container.functionDefStorage]
+      .filter((x) => !x[1].isCore)
+      .map((x) => x[1].toQ());
+    let qArr_scenario = [...this._builder.container.scenarioStorage]
+      .filter((x) => !x[1].isCore)
+      .map((x) => x[1].toQ());
+    
+    let qArr_full = [].concat(qArr_ns, qArr_unitDef, qArr_functionDef, qArr_scenario);
+
+    // remove unnecessary properties
+    let qArr = this.omit ? qArr_full.map((q) => omitByPaths(q, this.omit)) : qArr_full;
+
+    return [{
+      content: JSON.stringify(qArr, null, 2),
+      pathSuffix: '/output.json',
+      type: 'text'
+    }];
+  }
+}
+
+module.exports = CanonicalJSONExport;
