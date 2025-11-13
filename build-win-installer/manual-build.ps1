7bad32d6796f9dbf293b60e96aa38128416266b5
Evgeny Metelkin <evgeny.metelkin@gmail.com>
2025-11-13 14:52:32 +0200

    simplify MSI build

--- build-win-installer/manual-build.ps1
+++ build-win-installer/manual-build.ps1
@@ -10,15 +10,17 @@ $version = node -p "require('./package.json').version"
 $VERSION = $version
 
 # get new id
-$guid = [guid]::NewGuid().ToString()
-$NewGUID = $guid
+#$guid = [guid]::NewGuid().ToString()
+#$NewGUID = $guid
 
 # update version and id in .wxs file
-echo "GUID: $NewGUID"
+#echo "GUID: $NewGUID"
 $pathToWxs = "build-win-installer/perUser.wxs"
 $content = Get-Content $pathToWxs -Raw
-$newContent1 = $content -replace '(?<=Product\s+Id=")[^"]*', "$NewGUID"
-$newContent2 = $newContent1 -replace '(?<=Version\s*=\s*")0\.0\.0', "$VERSION"
+#$newContent1 = $content -replace '(?<=Product\s+Id=")[^"]*', "$NewGUID"
+#$newContent2 = $newContent1 -replace '(?<=Version\s*=\s*")0\.0\.0', "$VERSION"
+$newContent2 = $content -replace '(?<=Version\s*=\s*")0\.0\.0', "$VERSION"
+
 $newContent2 | Set-Content $pathToWxs
 
 # build .msi file for windows
