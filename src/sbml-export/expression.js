b6ecedee41bbde5bccf6efb023d1a8271b3c353a
Evgeny Metelkin <evgeny.metelkin@gmail.com>
2026-06-08 14:02:40 +0300

    fix user defined function <ci> in SBML

--- src/sbml-export/expression.js
+++ src/sbml-export/expression.js
@@ -6,9 +6,23 @@ const csymbols = {
   t: 'http://www.sbml.org/sbml/symbols/time'
 };
 
+// Custom handler for user defined functions in SBML
+// use <ci>fun1</ci> instead of <fun1/>
+function sbmlCMathMLHandler(node, options = {}) {
+  if (node.type === 'FunctionNode' && node.fnObj && !node.fnObj.isCore) {
+    let args = node.args
+      .map((arg) => arg.toString({ ...options, handler: sbmlCMathMLHandler }))
+      .join('');
+
+    return `<apply><ci>${node.fn.name}</ci>${args}</apply>`;
+  }
+
+  return cMathMLHandler(node, { ...options, handler: sbmlCMathMLHandler });
+}
+
 Expression.prototype.toCMathML = function(skipHeader = false){
   let baseMathML = this.exprParsed
-    .toString({ handler: cMathMLHandler, csymbols: csymbols });
+    .toString({ handler: sbmlCMathMLHandler, csymbols: csymbols });
 
   return !skipHeader
     ? `<math xmlns="http://www.w3.org/1998/Math/MathML">${baseMathML}</math>`
